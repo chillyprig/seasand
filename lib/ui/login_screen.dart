ab32b0e9a1f571066bdcedbba37df39b66da0ed3
Popeye <chillyprig@gmail.com>
2018-06-03 18:00:53 +0700

    Add simple login UI

---
+++ lib/ui/login_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'styles.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'Sea Sand app icon',
+      child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: Image.asset('assets/logo.png')),
+    );
+
+    final emptySpace = new Expanded(child: new Container());
+
+    final buttonLoginFacebook = new RaisedButton(
+      color: Styles.facebookColor,
+      textColor: Styles.whiteColor,
+      onPressed: () {
+        // TODO
+      },
+      child: new Text("Login with Facebook"),
+    );
+
+    return new Scaffold(
+        body: new Container(
+            padding: new EdgeInsets.all(32.0),
+            child: new Column(
+              children: <Widget>[
+                new Row(
+                  children: <Widget>[
+                    new Expanded(child: logo),
+                  ],
+                ),
+                emptySpace,
+                new Row(
+                  children: <Widget>[new Expanded(child: buttonLoginFacebook)],
+                )
+              ],
+            )));
+  }
+}
---
+++ lib/ui/styles.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class Styles {
+  static const facebookColor = const Color(0xFF3B5998);
+  static const whiteColor = const Color(0xFFFFFFFF);
+}
