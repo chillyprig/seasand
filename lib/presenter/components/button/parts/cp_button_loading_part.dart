17b1cb3612489696a03e6c74fe640b8f3e9e0547
Popeye <chillyprig@gmail.com>
2018-06-19 00:09:05 +0700

    Custom animation

--- lib/presenter/components/button/parts/cp_button_loading_part.dart
+++ lib/presenter/components/button/parts/cp_button_loading_part.dart
@@ -1,7 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:sea_sand/presenter/styles.dart';
 
-class CPButtonLoadingPart extends StatelessWidget {
+class CPButtonLoadingPart extends StatefulWidget {
   const CPButtonLoadingPart({
     Key key,
     this.backgroundColor: Styles.sandColor,
@@ -12,17 +14,68 @@ class CPButtonLoadingPart extends StatelessWidget {
   final Color fontColor;
 
   @override
-  Widget build(BuildContext context) {
-    return new Center(
-      child: new RaisedButton(
-        child: new CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation(fontColor),
-        ),
-        onPressed: null,
-        shape: new CircleBorder(),
-        disabledColor: backgroundColor,
-        disabledTextColor: fontColor,
-      ),
+  CPButtonLoadingPartState createState() {
+    return new CPButtonLoadingPartState();
+  }
+}
+
+class CPButtonLoadingPartState extends State<CPButtonLoadingPart>
+    with SingleTickerProviderStateMixin {
+
+  static const FACE_LEFT_ANGLE = pi / 2;
+
+  AnimationController animationController;
+  Animation animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = new AnimationController(
+        vsync: this,
+        duration: new Duration(milliseconds: 1000)
     );
+    animationController.addListener(() {
+      setState(() {});
+    });
+    animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animationController.forward(from: -pi * 2);
+      } else if (status == AnimationStatus.dismissed) {
+        animationController.forward();
+      }
+    });
+
+    Tween tween = new Tween<double>(
+      begin: 0.0,
+      end: pi * 2
+    );
+
+    final CurvedAnimation curve = new CurvedAnimation(parent: animationController, curve: Curves.easeInOut);
+
+    animation = tween.animate(curve);
+
+    animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return new Center(
+        child: new Transform.rotate(
+          angle: animation.value,
+          child: new Icon(
+            Icons.wb_sunny,
+            size: 80.0,
+            color: Colors.orangeAccent,
+          ),
+        )
+    );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 }
